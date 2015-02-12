430722368a7ed3e90d4e774a0e967a42bb10e511
silverspectro <decaro.ciro@gmail.com>
2015-02-12 20:56:00 +0100

    first attempt at navigation with url...don't understand

--- AngularSite/js/app.js
+++ AngularSite/js/app.js
@@ -1,43 +1,23 @@
 (function(){
-	var app = angular.module('site', [ ]);
+	var app = angular.module('site', [ 'ngRoute' ]);
 
 	//custom filter creating For loop with ng-repeat
 
-	app.filter('makeRange', function() {
-        return function(input) {
-        	if(!input)return false;
-            var lowBound, highBound;
-            switch (input.length) {
-            case 1:
-                lowBound = 0;
-                highBound = parseInt(input[0]) - 1;
-                break;
-            case 2:
-                lowBound = parseInt(input[0]);
-                highBound = parseInt(input[1]);
-                break;
-            default:
-                return input;
-            }
-            var result = [];
-            for (var i = lowBound; i <= highBound; i++)
-                result.push(i);
-            return result;
-        };
-    });
-
-    //end custom filter
-
-    app.controller('SiteController' , [ '$http', function($http){
+	app.controller('SiteController' , [ '$scope','$http', '$routeParams', function($scope, $http, $routeParams){
 		this.nav = nav;
 		this.current = 1;
 		var site = this;
 
 		site.articles = [ ];
+		site.articleId = 0;
 
-		$http.get('	http://localhost:8080/articles.json').success(function(articles){
+		site.articleId = $routeParams.id;
+
+		console.log(site.articleId);
+
+		$http.get('http://localhost:8080/articles.json').success(function(articles){
 			site.articles = articles;
-		});	
+		});
 
 		this.selectTab = function(currentNav) {
 			this.current = currentNav;
@@ -46,20 +26,84 @@
 		this.isSelected = function(checkNav) {
 			return this.current === checkNav;
 		};
-    } ]);
-    
+	} ]);
 
-    var nav = [
-	    {
-	    	id: 1,
-	    	name:"Main",
-	    	url:"index.html"
-	    },
-	    {	
-	    	id: 2,
-	    	name:"Articles",
-	    	url:"article.html"
-	    }
-    ];
+
+app.config(['$routeProvider',function($routeProvider) {
+    $routeProvider.
+      when('/articles/:id', {
+        templateUrl: 'directives/ind-article.html',
+				controller:"SiteController"
+      }).
+      otherwise({
+        redirectTo: '	/'
+      });
+			//$locationProvider.html5Mode(true);
+  }]);
+
+app.filter('makeRange', function() {
+      return function(input) {
+      	if(!input)return false;
+          var lowBound, highBound;
+          switch (input.length) {
+          case 1:
+              lowBound = 0;
+              highBound = parseInt(input[0]) - 1;
+              break;
+          case 2:
+              lowBound = parseInt(input[0]);
+              highBound = parseInt(input[1]);
+              break;
+          default:
+              return input;
+          }
+          var result = [];
+          for (var i = lowBound; i <= highBound; i++)
+              result.push(i);
+          return result;
+      };
+  });
+
+    //end custom filter
+
+		app.directive('listArticles', function(){
+			return {
+				restrict: "E",
+				templateUrl: "directives/list-articles.html"
+			}
+		});
+
+		app.directive('homeFeature', function(){
+			return {
+				restrict: "E",
+				templateUrl: "directives/home-feature.html"
+			}
+		});
+
+		app.directive('indArticle', function(){
+			return {
+				restrict: "E",
+				templateUrl: "directives/ind-article.html"
+			}
+		});
+
+
+  var nav = [
+    {
+    	id: 1,
+    	name:"Main",
+    	url:"index.html"
+    },
+    {
+    	id: 2,
+    	name:"Articles",
+    	url:"article.html"
+    },
+		{
+			id: 3,
+			name:"Projects",
+			url:"project.html"
+		}
+  ];
 
 })();
